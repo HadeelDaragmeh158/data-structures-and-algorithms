000b4d9abbece5cb3a9e774f5b63f60acfdb24ad
HadeelDaragmeh158 <hadeelmohammad158@gmail.com>
2022-03-31 17:17:40 +0300

    tree

---
+++ Challenge15/app/src/main/java/trees/BinaryTree.java
@@ -0,0 +1,79 @@
+package trees;
+
+import java.util.ArrayList;
+
+
+public class BinaryTree<T extends Comparable<T>>{
+
+    private Node root;
+    ArrayList<Integer> preOrder = new ArrayList<>();
+    ArrayList<Integer> inOrder = new ArrayList<>();
+    ArrayList<Integer> postOrder = new ArrayList<>();
+
+    public BinaryTree(Node root) {
+        this.root = root;
+    }
+    BinaryTree() { root = null; }
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public ArrayList<Integer> getPreOrder() {
+        return preOrder;
+    }
+
+    public ArrayList<Integer> getInOrder() {
+        return inOrder;
+    }
+
+    public ArrayList<Integer> getPostOrder() {
+        return postOrder;
+    }
+
+/*
+//_____________________________________________________________________________
+//_____________________________________________________________________________
+//_____________________________________________________________________________
+ */
+    public void preOrder(Node node) {
+        if (root == null)
+            return;
+        if(node!=null) {
+            preOrder.add(node.getData());
+            preOrder(node.getLeftNode());
+            preOrder(node.getRightNode());
+        }
+
+    }
+    public void inOrder(Node node){
+        if (node == null)
+            return;
+        else {
+            inOrder(node.getLeftNode());
+            inOrder.add(node.getData());
+            inOrder(node.getRightNode());
+        }
+    }
+    public void postOrder(Node node){
+        if (node == null)
+            return;
+        else{
+            postOrder(node.getLeftNode());
+            postOrder(node.getRightNode());
+            postOrder.add(node.getData());
+        }
+
+    }
+
+    @Override
+    public String toString() {
+        return "BinaryTree{" +
+                "root " + root +
+                '}';
+    }
+}
